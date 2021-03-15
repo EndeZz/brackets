b5aa6fea9a79d2da00b0c0281354505ba914b13e
endezz <r-serega13@yandex.ru>
2021-03-15 17:25:49 +0400

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const open = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+    };
+    const closed = {
+        "}": true,
+        "]": true,
+        ")": true,
+    };
+    let static = [];
+    for (let i = 0; i < str.length; i++) {
+        if (open[str[i]]) {
+            static.push(str[i]);
+        } else if (closed[str[i]]) {
+            if (open[static.pop()] !== str[i]) return false;
+        }
+    }
+    if (static % 2 == 0 && str.indexOf("|") != -1) return true;
+    return static.length === 0;
+};
